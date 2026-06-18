18aac58d8ef181515f49e5efe4c4e1db04e28e50
LEon3209 <93526230+LEon3209@users.noreply.github.com>
2026-06-18 16:49:01 +0200

    Delete files/Projects/04_Web-Dev/Blog-Website-Django directory

--- files/Projects/04_Web-Dev/Blog-Website-Django/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('post_list/', views.post_list, name='post_list'),
-    path('create_blog/', views.create_blog, name='create_blog'),
-]
